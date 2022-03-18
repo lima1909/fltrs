7f0ca47e93170b16f424d157748474fa0a373b3f
lima1909 <linke.mario@web.de>
2022-03-18 16:17:24 +0100

    idea2 with PathResolver as arg

--- src/idea2.rs
+++ src/idea2.rs
@@ -1,44 +1,51 @@
-use crate::{operator::OperatorFn, value::Value, PathResolver};
+use crate::{operator::OperatorFn, value::Value, Filterable};
 
-pub type PredicateFn<'a> = &'a dyn Fn(&dyn PathResolver) -> bool;
+pub type Args<'a> = &'a [&'a dyn Filterable];
+pub type PredicateFn<'a> = &'a dyn Fn(Args<'a>) -> bool;
 
-pub fn predicate(pos: usize, f: OperatorFn, val: Value) -> impl Fn(&dyn PathResolver) -> bool {
-    move |arg: &dyn PathResolver| -> bool { f(arg.value(pos), &val) }
+pub fn predicate<'a>(pos: usize, f: OperatorFn, val: Value) -> impl Fn(Args<'a>) -> bool + 'a {
+    move |arg: Args<'a>| -> bool { f(arg[pos], &val) }
 }
 
-pub fn or<'a>(l: PredicateFn<'a>, r: PredicateFn<'a>) -> impl Fn(&dyn PathResolver) -> bool + 'a {
-    move |arg: &dyn PathResolver| -> bool { (l)(arg) || (r)(arg) }
+pub fn or<'a>(
+    l: impl Fn(Args<'a>) -> bool + 'a,
+    r: impl Fn(Args<'a>) -> bool + 'a,
+) -> impl Fn(Args<'a>) -> bool + 'a {
+    move |arg: Args<'a>| -> bool { (l)(arg) || (r)(arg) }
 }
 
-pub fn and<'a>(l: PredicateFn<'a>, r: PredicateFn<'a>) -> impl Fn(&dyn PathResolver) -> bool + 'a {
-    move |arg: &dyn PathResolver| -> bool { (l)(arg) && (r)(arg) }
+pub fn and<'a>(
+    l: impl Fn(Args<'a>) -> bool + 'a,
+    r: impl Fn(Args<'a>) -> bool + 'a,
+) -> impl Fn(Args<'a>) -> bool + 'a {
+    move |arg: Args<'a>| -> bool { (l)(arg) && (r)(arg) }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::{operator::Operators, value::Number, Filterable};
+    use crate::{operator::Operators, value::Number};
 
     struct Example {
         name: String,
         x: i32,
     }
 
-    impl PathResolver for Example {
-        fn path_to_index(&self, path: &str) -> Option<usize> {
-            match path {
-                "x" => Some(1),
-                _ => Some(0),
-            }
-        }
+    // impl crate::PathResolver for Example {
+    //     fn path_to_index(&self, path: &str) -> Option<usize> {
+    //         match path {
+    //             "x" => Some(1),
+    //             _ => Some(0),
+    //         }
+    //     }
 
-        fn value(&self, idx: usize) -> &dyn Filterable {
-            match idx {
-                1 => &self.x,
-                _ => &self.name,
-            }
-        }
-    }
+    //     fn value(&self, idx: usize) -> &dyn crate::Filterable {
+    //         match idx {
+    //             1 => &self.x,
+    //             _ => &self.name,
+    //         }
+    //     }
+    // }
 
     #[test]
     fn first() {
@@ -46,22 +53,29 @@ mod test {
             name: String::from("Paul"),
             x: 42,
         };
+        let args: Args = &[&e.name, &e.x];
 
         let op = Operators::default();
 
-        let name = predicate(0, op.get("!=").unwrap(), Value::Text(String::from("Paul")));
-        assert!(!(name)(&e));
+        let paul = predicate(0, op.get("!=").unwrap(), Value::Text(String::from("Paul")));
+        assert!(!(paul)(args));
 
-        let x = predicate(1, op.get("!=").unwrap(), Value::Number(Number::I32(7)));
-        assert!((x)(&e));
+        let x7 = predicate(1, op.get("!=").unwrap(), Value::Number(Number::I32(7)));
+        assert!((x7)(args));
 
-        let or1 = or(&name, &x);
-        assert!((or1)(&e));
+        let or1 = or(paul, x7);
+        assert!((or1)(args));
 
-        let or2 = or(&name, &or1);
-        assert!((or2)(&e));
+        let inge = predicate(0, op.get("=").unwrap(), Value::Text(String::from("Inge")));
+        assert!(!(inge)(args));
 
-        let and = and(&or1, &or2);
-        assert!((and)(&e));
+        let or2 = or(inge, or1);
+        assert!((or2)(args));
+
+        let x5 = predicate(1, op.get("!=").unwrap(), Value::Number(Number::I32(5)));
+        assert!((x5)(args));
+
+        let and = and(x5, or2);
+        assert!((and)(args));
     }
 }
